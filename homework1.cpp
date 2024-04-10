0eab00035fa06090a93fdabe1142db22ed698eca
savaty19 <165250480+savaty19@users.noreply.github.com>
2024-04-10 22:19:48 +0700

    Add files via upload

---
+++ homework1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int Number1;
+	int Number2;
+	int Total;
+	cout << "\nplease input the Number :";
+	cin >> Number1;
+	cout << "\ninput the Number :";
+	cin >> Number2;
+	Total = Number1 + Number2;
+	cout <<"Total :" << Total;
+	
+	
+	
+	return 0;
+}
